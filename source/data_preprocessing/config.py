7989d93ee9c363548c30007bf0770ad81f62f5ed
MisterFOURXXX <fourapiwit@gmail.com>
2026-08-10 12:41:40 +0200

    Fix errors in data preprocessing

--- source/data_preprocessing/config.py
+++ source/data_preprocessing/config.py
@@ -1,7 +1,7 @@
 import sys
 import os
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
-sys.path.insert(0, project_root)   # <-- insert the project root
+#project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+#sys.path.insert(0, project_root)   # <-- insert the project root
 
 # Default paths – these are absolute and unchanged
 DEFAULT_ZIP_PATH = "/kaggle/working/deepseekmoe_dynamic_routing_algorithms/dataset/MultiWOZ-coref/MultiWOZ2_3.zip"
--- source/data_preprocessing/preprocessing.py
+++ source/data_preprocessing/preprocessing.py
@@ -1,8 +1,7 @@
 import sys
 import os
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
-sys.path.insert(0, project_root)   # <-- insert the project root
-
+#project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+#sys.path.insert(0, project_root)
 import re
 import json
 import random
