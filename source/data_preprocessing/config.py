fcd819ef50eedb2371efdf5405806da3f98e57e1
MisterFOURXXX <fourapiwit@gmail.com>
2026-08-10 11:38:12 +0200

    Fix errors in data preprocessing

--- source/data_preprocessing/config.py
+++ source/data_preprocessing/config.py
@@ -1,3 +1,9 @@
+import os
+import sys
+repo_path =  ".."
+os.chdir(repo_path)                 # Move into the repo
+sys.path.insert(0, os.getcwd())     # Ensure the repo root is on sys.path
+
 # Default paths (adjust as needed)
 DEFAULT_ZIP_PATH = "/kaggle/working/deepseekmoe_dynamic_routing_algorithms/dataset/MultiWOZ-coref/MultiWOZ2_3.zip"
 DEFAULT_TRAIN_PATH = "/kaggle/working/deepseekmoe_dynamic_routing_algorithms/dataset/train_sequences.txt"
