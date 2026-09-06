4a9adbd5e004ae7b10e75a32d46dcd74285df631
MisterFOURXXX <fourapiwit@gmail.com>
2026-09-07 01:31:40 +0200

    Revise data preprocessing script

--- source/data_preprocessing/preprocessing.py
+++ source/data_preprocessing/preprocessing.py
@@ -9,7 +9,7 @@ import dateparser
 from word2number import w2n
 from sklearn.model_selection import train_test_split
 
-from source.data_preprocessing.config import (
+from deepseekmoe_dynamic_routing_algorithms.source.data_preprocessing.config import (
     DEFAULT_EVAL_PATH,
     DEFAULT_FINE_PATH,
     DEFAULT_TRAIN_PATH,
